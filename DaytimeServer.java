255b122b6d9ab7c173828f538b3258db2f822740
Sreenu Sasubilli <sasubillis@gmail.com>
2014-02-20 11:44:42 +0200

    Reading command line port number specified

--- DaytimeServer.java
+++ DaytimeServer.java
@@ -1,5 +1,30 @@
 package assignment;
 
-public class DaytimeServer {
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
 
+public class DaytimeServer {
+	public static void main(String args[]) {
+		try {
+			CharsetEncoder encoder = Charset.forName("US-ASCII").newEncoder();
+
+			// Read the port number from command line argument
+			int port = 13; // default port.
+			if (args.length > 0) {
+			    try {
+			        port = Integer.parseInt(args[0]);
+			    } catch (NumberFormatException e) {
+			        System.err.println("Argument" + " must be an integer");
+			        System.exit(1);
+			    }
+			}
+
+			SocketAddress localport = new InetSocketAddress(port);
+		} catch (Exception e) {
+			System.err.println(e);
+			System.exit(1);
+		}
+	}
 }
