70b024b1beb16864c86123e0993d08f6bafdd570
Sreenu Sasubilli <sasubillis@gmail.com>
2014-02-20 11:52:25 +0200

    Creating TCP and UDP channel using nio

--- DaytimeServer.java
+++ DaytimeServer.java
@@ -2,6 +2,8 @@ package assignment;
 
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
+import java.nio.channels.DatagramChannel;
+import java.nio.channels.ServerSocketChannel;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetEncoder;
 
@@ -13,15 +15,25 @@ public class DaytimeServer {
 			// Read the port number from command line argument
 			int port = 13; // default port.
 			if (args.length > 0) {
-			    try {
-			        port = Integer.parseInt(args[0]);
-			    } catch (NumberFormatException e) {
-			        System.err.println("Argument" + " must be an integer");
-			        System.exit(1);
-			    }
+				try {
+					port = Integer.parseInt(args[0]);
+				} catch (NumberFormatException e) {
+					System.err.println("Argument" + " must be an integer");
+					System.exit(1);
+				}
 			}
 
 			SocketAddress localport = new InetSocketAddress(port);
+			// TCP Channel
+			ServerSocketChannel tcpserver = ServerSocketChannel.open();
+			tcpserver.socket().bind(localport);
+			tcpserver.configureBlocking(false);
+
+			// UDP Channel
+			DatagramChannel udpserver = DatagramChannel.open();
+			udpserver.socket().bind(localport);
+			udpserver.configureBlocking(false);
+
 		} catch (Exception e) {
 			System.err.println(e);
 			System.exit(1);
