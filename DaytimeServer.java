d5fc7b5133ada518fc0f8f64c862e9ca963c8959
Sreenu Sasubilli <sasubillis@gmail.com>
2014-02-20 12:06:43 +0200

    in event loop prepare daytime message from Date()

--- DaytimeServer.java
+++ DaytimeServer.java
@@ -2,7 +2,11 @@ package assignment;
 
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.channels.DatagramChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetEncoder;
@@ -33,6 +37,27 @@ public class DaytimeServer {
 			DatagramChannel udpserver = DatagramChannel.open();
 			udpserver.socket().bind(localport);
 			udpserver.configureBlocking(false);
+
+			// Block the channel while waiting actively for data
+			Selector selector = Selector.open();
+			tcpserver.register(selector, SelectionKey.OP_ACCEPT);
+			udpserver.register(selector, SelectionKey.OP_READ);
+
+			// Create buffer to receive udp inet adderss
+			ByteBuffer receiveBuffer = ByteBuffer.allocate(0);
+
+			// infinite loop waiting for connections
+			for (;;) {
+				try {
+					selector.select(); // blocking call
+					// Prepare the daytime server response to connected client
+					String date = new java.util.Date().toString() + "\r\n";
+					ByteBuffer response = encoder.encode(CharBuffer.wrap(date));
+
+				} catch (java.io.IOException e) {
+					System.err.println(e);
+				}
+			}
 
 		} catch (Exception e) {
 			System.err.println(e);
